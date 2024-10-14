5aae3682424cfc831bc675d2cc57ebcb4a8bf425
Arian <arian.koochak@protonmail.com>
2024-10-14 10:43:07 +0330

    update loginInput style and add placeholder

--- frontend/src/components/LoginInput/LoginInput.jsx
+++ frontend/src/components/LoginInput/LoginInput.jsx
@@ -5,8 +5,8 @@ export default function LoginInput() {
   return (
       <div className='login-input'>
         <h3>Darhamarket</h3>
-        <input type="text" />
-        <input type="password" />
+        <input type="text" placeholder='Email'/>
+        <input type="password" placeholder='password'/>
         <button>Login</button>
         <span className='text'>Dont have and Account? <span className='sign-up'>Sign Up</span></span>
       </div>
