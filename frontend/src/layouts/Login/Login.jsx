0e0cd1ce7b85bbc812c20722c13a3402a6290607
Arian <arian.koochak@protonmail.com>
2024-10-14 10:44:15 +0330

    add loginInput to login layout

--- frontend/src/layouts/Login/Login.jsx
+++ frontend/src/layouts/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import LoginInput from '../../components/LoginInput/LoginInput'
+import '../../assets/styles/login.style.css'
 
 export default function Login() {
   return (
-    <div>
-      <LoginInput/>
+    <div className='login-layout'>
+        <LoginInput/>
     </div>
   )
 }
