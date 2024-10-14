1a58b28dab54629a8d1c4e0886392ae80f4fec16
Arian <arian.koochak@protonmail.com>
2024-10-14 13:27:27 +0330

    add route for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Login from './layouts/Login/Login'
 import SignUp from "./layouts/SignUp/SignUp";
+import Home from './layouts/Home/Home';
+import ProfileSetting from "./layouts/ProfileSetting/ProfileSetting";
+import Categories from "./layouts/Categories/Categories";
+import Basket from "./layouts/Basket/Basket";
+
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -8,8 +13,12 @@ export default function App() {
   return (
       <BrowserRouter>
           <Routes>
+              <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/profile" element={<ProfileSetting />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/basket" element={<Basket />} />
           </Routes>
       </BrowserRouter>
   );
