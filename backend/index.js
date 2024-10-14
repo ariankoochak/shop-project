e25f8813996aff9415a56bf64199005c6dbcb7ef
Arian <arian.koochak@protonmail.com>
2024-10-14 15:18:22 +0330

    add catching errors middleware

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,15 @@ require('./src/config/mongoose/mongoose.config')
 
 const allRoutes = require('./src/modules/routes/routes');
 
-app.use(allRoutes)
+app.use(allRoutes);
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({
+        error: {
+            message: err.message || "internal server error",
+        },
+    });
+});
 
 app.listen(PORT,()=>{
     console.log(chalk.blue("\nserver started on http://localhost:3000\n"));
