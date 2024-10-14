6158c5a611a02c765ce5ad2cbfd1b05e17274d33
Arian <arian.koochak@protonmail.com>
2024-10-14 12:42:24 +0330

    install react router dom and add to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Login from './layouts/Login/Login'
+import SignUp from "./layouts/SignUp/SignUp";
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
-    <Login/>
-  )
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+      </BrowserRouter>
+  );
 }
