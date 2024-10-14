73dd0e1363303fd3248a582657ad4965251713de
Arian <arian.koochak@protonmail.com>
2024-10-14 10:50:52 +0330

    add controlled input for loginInput

--- frontend/src/components/LoginInput/LoginInput.jsx
+++ frontend/src/components/LoginInput/LoginInput.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../assets/styles/loginInput.style.css'
 
 export default function LoginInput() {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  const handleChangeEmailInput = (e)=>{
+    setEmail(e.target.value);
+  }
+
+  const handleChangePasswordInput = (e)=>{
+    setPassword(e.target.value);
+  }
   return (
       <div className='login-input'>
         <h3>Darhamarket</h3>
-        <input type="text" placeholder='Email'/>
-        <input type="password" placeholder='password'/>
+        <input type="text" value={email} placeholder='Email' onChange={handleChangeEmailInput}/>
+        <input type="password" value={password} placeholder='password' onChange={handleChangePasswordInput}/>
         <button>Login</button>
         <span className='text'>Dont have and Account? <span className='sign-up'>Sign Up</span></span>
       </div>
