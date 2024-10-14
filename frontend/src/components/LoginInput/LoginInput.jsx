087d7b6fcbadf417fd1a6bd785fda33ff0aa5038
Arian <arian.koochak@protonmail.com>
2024-10-14 12:44:55 +0330

    add navigation in login input to sign up layout

--- frontend/src/components/LoginInput/LoginInput.jsx
+++ frontend/src/components/LoginInput/LoginInput.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import '../../assets/styles/loginInput.style.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginInput() {
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleChangeEmailInput = (e)=>{
     setEmail(e.target.value);
@@ -12,13 +14,18 @@ export default function LoginInput() {
   const handleChangePasswordInput = (e)=>{
     setPassword(e.target.value);
   }
+
+  const handleClickSignUp = ()=>{
+    navigate('/sign-up');
+  }
+  
   return (
       <div className='login-input'>
         <h3>Darhamarket</h3>
         <input type="text" value={email} placeholder='Email' onChange={handleChangeEmailInput}/>
         <input type="password" value={password} placeholder='password' onChange={handleChangePasswordInput}/>
         <button>Login</button>
-        <span className='text'>Dont have and Account? <span className='sign-up'>Sign Up</span></span>
+        <span className='text'>Dont have and Account? <span className='sign-up' onClick={handleClickSignUp}>Sign Up</span></span>
       </div>
   );
 }
