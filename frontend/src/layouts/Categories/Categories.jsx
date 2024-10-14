52839fd75f56463f1de8c17964b2e66bb56efbcf
Arian <arian.koochak@protonmail.com>
2024-10-14 14:17:19 +0330

    make categories and add images

--- frontend/src/layouts/Categories/Categories.jsx
+++ frontend/src/layouts/Categories/Categories.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import CategoryCard from '../../components/CategoryCard/CategoryCard'
+import cpuImageSrc from '../../assets/images/cpu.jpg'
+import gpuImageSrc from "../../assets/images/graphicCard.jpg";
+import caseImageSrc from "../../assets/images/case.jpg";
+import motherboardImageSrc from "../../assets/images/motherboard.jpg";
+import '../../assets/styles/categories.style.css'
+
 
 export default function Categories() {
   return (
-    <>
-      <Navbar selectedPage={'categories'}/>
-    </>
-  )
+      <div className="categories">
+          <Navbar selectedPage={"categories"} />
+          <div className="category-cards-container">
+              <CategoryCard categoryName={"Cpu"} categoryImageSrc={cpuImageSrc} />
+              <CategoryCard categoryName={"Graphic Card"} categoryImageSrc={gpuImageSrc} />
+              <CategoryCard categoryName={"Case"} categoryImageSrc={caseImageSrc} />
+              <CategoryCard categoryName={"Motherboard"} categoryImageSrc={motherboardImageSrc} />
+          </div>
+      </div>
+  );
 }
