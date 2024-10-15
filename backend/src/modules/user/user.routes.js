417f8fddf5776552e348f9f1e48981c43d1795a4
Arian <arian.koochak@protonmail.com>
2024-10-15 16:49:59 +0330

    fix route

--- backend/src/modules/user/user.routes.js
+++ backend/src/modules/user/user.routes.js
@@ -7,7 +7,7 @@ router.get("/auth",userController.authentication);
 router.get("/reset-pass",userController.resetPassword);
 router.post("/basket/add", userController.addToBasket);
 router.post("/basket/remove", userController.removeFromBasket);
-router.get("/basket/",userController.getBasket)
+router.get("/basket",userController.getBasket);
 
 
 module.exports = router;
