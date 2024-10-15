6df2223ca8bd2eb756721b96aab9beb876d60f34
Arian <arian.koochak@protonmail.com>
2024-10-15 12:20:23 +0330

    add product routes

--- backend/src/modules/routes/routes.js
+++ backend/src/modules/routes/routes.js
@@ -2,8 +2,11 @@ const { Router } = require("express");
 const router = Router();
 
 const userRouter = require('../user/user.routes')
+const productRouter = require('../product/product.routes')
 
 router.use("/user", userRouter);
+router.use("/product", productRouter);
+
 
 
 module.exports = router;
