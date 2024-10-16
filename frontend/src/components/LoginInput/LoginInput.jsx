911b6a3218f25362f14421942903aca6987765a7
Arian <arian.koochak@protonmail.com>
2024-10-16 09:30:42 +0330

    install axios & add login api

--- frontend/src/components/LoginInput/LoginInput.jsx
+++ frontend/src/components/LoginInput/LoginInput.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import '../../assets/styles/loginInput.style.css'
 import { useNavigate } from 'react-router-dom';
 import {isEmail} from 'valiend'
+import axios from 'axios'
+import { useDispatch } from "react-redux";
+import { setUserData } from '../../utils/store/slices/userData';
 
-//TODO: after make API in backend => add authentication and validation datas for login
+
 export default function LoginInput() {
+  const BACKEND_URL = 'http://localhost:3000'
   const [email,setEmail] = useState('');
   const [password,setPassword] = useState('');
   const [err,setErr] = useState('');
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChangeEmailInput = (e)=>{
     setEmail(e.target.value);
@@ -24,7 +29,36 @@ export default function LoginInput() {
 
   const handleClickLoginButton = ()=>{
     if(isEmail(email)){
-        
+        const API = `${BACKEND_URL}/user/auth`;
+         const dataObj = JSON.parse(`{
+                "email": "${email}",
+                "password": "${password}"
+          }`);
+         axios({
+             headers: {
+                 "content-type": "application/json",
+             },
+             withCredentials: true,
+             data: dataObj,
+             method: "post",
+             url: API,
+         })
+             .then((response) => {
+                 if (response.status === 200) {
+                    dispatch(setUserData({
+                      id : response.data._id,
+                      email : response.data.email,
+                      password : password,
+                      liveBasket : response.data.liveBasket,
+                    }))
+                    navigate('/')
+                 }
+             })
+             .catch((error) => {
+                 if (error.response.status === 401) {
+                     setErr("username,email or password is wrong");
+                 }
+             });
     }
     else{
       setErr('invalid email')
