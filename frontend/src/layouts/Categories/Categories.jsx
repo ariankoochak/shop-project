c3ca44e1c971257c95ea71c0ac599494df15aaef
Arian <arian.koochak@protonmail.com>
2024-10-16 11:39:43 +0330

    add categories api

--- frontend/src/layouts/Categories/Categories.jsx
+++ frontend/src/layouts/Categories/Categories.jsx
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Products from '../Products/Products'
 import CategoryCard from '../../components/CategoryCard/CategoryCard'
 import cpuImageSrc from '../../assets/images/cpu.jpg'
 import gpuImageSrc from "../../assets/images/graphicCard.jpg";
 import caseImageSrc from "../../assets/images/case.jpg";
 import motherboardImageSrc from "../../assets/images/motherboard.jpg";
-import '../../assets/styles/categories.style.css'
+import '../../assets/styles/categories.style.css';
+import axios from 'axios'
 
 
 export default function Categories() {
+    const BACKEND_URL = "http://localhost:3000";
+    const [products,setProducts] = useState([]);
+    const handleClickCategoryCard = (category)=>{
+        const API = `${BACKEND_URL}/product/category/${category}`;
+        axios({
+            withCredentials: true,
+            method: "get",
+            url: API,
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    setProducts(response.data)
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
   return (
       <div className="categories">
           <Navbar selectedPage={"categories"} />
-          <div className="category-cards-container">
-              <CategoryCard categoryName={"Cpu"} categoryImageSrc={cpuImageSrc} />
-              <CategoryCard categoryName={"Graphic Card"} categoryImageSrc={gpuImageSrc} />
-              <CategoryCard categoryName={"Case"} categoryImageSrc={caseImageSrc} />
-              <CategoryCard categoryName={"Motherboard"} categoryImageSrc={motherboardImageSrc} />
-          </div>
+              <div className="category-cards-container">
+                  <span onClick={() => handleClickCategoryCard("CPU")}>
+                      <CategoryCard
+                          categoryName={"Cpu"}
+                          categoryImageSrc={cpuImageSrc}
+                      />
+                  </span>
+                  <span onClick={() => handleClickCategoryCard("GPU")}>
+                      <CategoryCard
+                          categoryName={"Graphic Card"}
+                          categoryImageSrc={gpuImageSrc}
+                      />
+                  </span>
+                  <span onClick={() => handleClickCategoryCard("Case")}>
+                      <CategoryCard
+                          categoryName={"Case"}
+                          categoryImageSrc={caseImageSrc}
+                      />
+                  </span>
+                  <span onClick={() => handleClickCategoryCard("Motherboard")}>
+                      <CategoryCard
+                          categoryName={"Motherboard"}
+                          categoryImageSrc={motherboardImageSrc}
+                      />
+                  </span>
+              </div>
+          {products.length > 0 && <Products productsList = {products}/>}
       </div>
   );
 }
--- frontend/src/layouts/Products/Products.jsx
+++ frontend/src/layouts/Products/Products.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import '../../assets/styles/products.style.css'
 
-export default function Products() {
+export default function Products({productsList}) {
+  const renderProducts = ()=>{
+    return productsList.map((product)=>{
+      return(
+        <div className="product" key={product._id}>
+            <img src={`http://localhost:3000/${product.imageName}.jpg`} alt=""/>
+            <span className='name'>{product.name}</span>
+            <span className='price'>price : {product.price}T</span>
+        </div>)
+    })
+  }
   return (
-    <div>Products</div>
+    <div className='products-container'>{renderProducts()}</div>
   )
 }
