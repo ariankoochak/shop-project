a71e3d91de976a61652bb4aeae79342e3d5b2995
Arian <arian.koochak@protonmail.com>
2024-10-16 09:58:32 +0330

    add sign up api

--- frontend/src/components/SignUpInput/SignUpInput.jsx
+++ frontend/src/components/SignUpInput/SignUpInput.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../../assets/styles/signUpInput.style.css'
+import { isEmail } from 'valiend';
+import axios from 'axios';
 
-//TODO: after make API in backend => add authentication and validation datas for sign up
 export default function SignUpInput() {
+    const BACKEND_URL = "http://localhost:3000";
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [err,setErr] = useState("")
@@ -26,6 +28,50 @@ export default function SignUpInput() {
         navigate('/login')
     }
 
+    const handleClickSignUp = ()=>{
+        if(isEmail(email)){
+            if(password === retypePassword){
+                const API = `${BACKEND_URL}/user/create`;
+                const dataObj = JSON.parse(`{
+                "email": "${email}",
+                "password": "${password}"
+                }`);
+                axios({
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    withCredentials: true,
+                    data: dataObj,
+                    method: "post",
+                    url: API,
+                })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            dispatch(
+                                setUserData({
+                                    id: response.data._id,
+                                    email: response.data.email,
+                                    password: password,
+                                    liveBasket: response.data.liveBasket,
+                                })
+                            );
+                            navigate("/");
+                        }
+                    })
+                    .catch((error) => {
+                        if (error.response.status === 409) {
+                            setErr("email already exist");
+                        }
+                    });
+            }
+            else{
+                setErr('passwords dont match')
+            }
+        }
+        else{
+            setErr('invalid email')
+        }
+    }
 
   return (
     <div className='sign-up-input'>
@@ -34,7 +80,7 @@ export default function SignUpInput() {
         <input type="text" value={email} placeholder='Email' onChange={handleChangeEmailInput}/>
         <input type="password" value={password} placeholder='password' onChange={handleChangePasswordInput}/>
         <input type="password" value={retypePassword} placeholder='retype password' onChange={handleChangeRetypePassword}/>
-        <button>Sign Up</button>
+        <button onClick={handleClickSignUp}>Sign Up</button>
         <span className='text'>Already have and Account? <span className='sign-up' onClick={handleClickLogin}>Login</span></span>
       </div>
   )
