fb81161c065301fdbacd8e132827284423fb50c1
Arian <arian.koochak@protonmail.com>
2024-10-16 09:31:38 +0330

    change get to post method

--- backend/src/modules/user/user.routes.js
+++ backend/src/modules/user/user.routes.js
@@ -3,8 +3,8 @@ const userController = require("./user.controller");
 const router = Router();
 
 router.post("/create",userController.create);
-router.get("/auth",userController.authentication);
-router.get("/reset-pass",userController.resetPassword);
+router.post("/auth",userController.authentication);
+router.post("/reset-pass",userController.resetPassword);
 router.post("/basket/add", userController.addToBasket);
 router.post("/basket/remove", userController.removeFromBasket);
 router.get("/basket",userController.getBasket);
