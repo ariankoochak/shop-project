06b78fe6f11fcb21473e33110ea1b28095a501a2
Arian <arian.koochak@protonmail.com>
2024-10-16 07:36:10 +0330

    add navigate to login

--- frontend/src/layouts/Home/Home.jsx
+++ frontend/src/layouts/Home/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 export default function Home() {
+  const userData = useSelector((state) => state.userData.userData);
+  const navigate = useNavigate();
+  useEffect(()=>{
+    if(Object.keys(userData).length === 0){
+      navigate('/Login');
+    }
+  },[])
   return (
     <>
       <Navbar selectedPage={'home'}/>
