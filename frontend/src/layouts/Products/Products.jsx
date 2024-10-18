b2cd899a0f5e11c17a1785a024824b840f4a8b8e
Arian <arian.koochak@protonmail.com>
2024-10-18 09:26:55 +0330

    add go to product page and get product data

--- frontend/src/layouts/Products/Products.jsx
+++ frontend/src/layouts/Products/Products.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import '../../assets/styles/products.style.css'
+import { useDispatch } from 'react-redux';
+import { setOpenProductData } from "../../utils/store/slices/openProductData";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Products({productsList}) {
+  const BACKEND_URL = "http://localhost:3000";
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleClickProduct = (productId)=>{
+      const API = `${BACKEND_URL}/product/id/${productId}`;
+      axios({
+          withCredentials: true,
+          method: "get",
+          url: API,
+      })
+          .then((response) => {
+              if (response.status === 200) {
+                  dispatch(
+                      setOpenProductData({
+                          id: response.data._id,
+                          name: response.data.name,
+                          price: response.data.price,
+                          category : response.data.category,
+                          imageName: response.data.imageName,
+                      })
+                  );
+                  navigate("/product");
+              }
+          })
+          .catch((error) => {
+                  console.log(error);
+          });
+  }
+
   const renderProducts = ()=>{
     return productsList.map((product)=>{
       return(
-        <div className="product" key={product._id}>
+        <div className="product" key={product._id} onClick={()=>handleClickProduct(product._id)}>
             <img src={`http://localhost:3000/${product.imageName}.jpg`} alt=""/>
             <span className='name'>{product.name}</span>
             <span className='price'>price : {product.price}T</span>
