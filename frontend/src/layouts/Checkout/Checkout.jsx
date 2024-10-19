7627989cea5dc81fea637546e3735eed89425b2a
Arian <arian.koochak@protonmail.com>
2024-10-19 10:56:11 +0330

    add checkout page

--- frontend/src/layouts/Checkout/Checkout.jsx
+++ frontend/src/layouts/Checkout/Checkout.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import '../../assets/styles/checkout.style.css'
+import { useDispatch } from 'react-redux';
+import { updateBasket } from '../../utils/store/slices/userData';
+import { useSelector } from "react-redux";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function Checkout() {
+  const userData = useSelector((state) => state.userData.userData);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(()=>{
+  const BACKEND_URL = "http://localhost:3000";
+    const API = `${BACKEND_URL}/user/basket/${userData.email}`;
+    axios({
+        withCredentials: true,
+        method: "delete",
+        url: API,
+    })
+        .then((response) => {
+            if (response.status === 201) {
+                dispatch(updateBasket([]));
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+  })
+
+  const handleClickBackToHomePageButton = ()=>{
+    navigate('/')
+  }
   return (
-    <div>Checkout</div>
-  )
+      <div className='checkout-page'>
+          <h1>Your purchase was successful!</h1>
+          <h3 onClick={handleClickBackToHomePageButton}>back to Home Page</h3>
+      </div>
+  );
 }
